3cc159bb89f226913588693b784d1ccb2772cf88
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2015-11-11 11:55:26 -0500

    Updated example001

--- examples/example001/generate_word_cloud_for_bob_murphy.py
+++ examples/example001/generate_word_cloud_for_bob_murphy.py
@@ -1,3 +1,16 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from os import path
 import os
 from scipy.misc import imread
@@ -19,15 +32,18 @@ def grey_color_func(word, font_size, position, orientation, random_state=None, *
 
 d = os.getcwd()
 
-# little women by louisa may alcott
+#these are the extracted words from bob murphy's article titles
 filename = 'words.txt'
 text = open(filename).read()
 
 stopwords = STOPWORDS.copy()
 
+width=2400
+height=2200
+
 for number_of_words in list_of_number_of_words:
 	wordcloud = WordCloud(max_words=number_of_words, stopwords=stopwords, margin=0,
-	      	        random_state=1).generate(text)
+	      	        random_state=1, width=width, height=height).generate(text)
 
 	filename='words' + str(number_of_words).zfill(6) + '.png'
 	image = wordcloud.to_image()
--- examples/example001/generate_word_cloud_for_bob_murphy.sh
+++ examples/example001/generate_word_cloud_for_bob_murphy.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
-for I in {1..500}
-do
-	URL='https://scholar.google.com/citations?user=qQLlBH4AAAAJ&hl=en&oi=ao&cstart='
-	wget -nc --output-document "$I.html" $URL$I
-	cat $I.html | tr ">" "\n" >> temp.html
-	rm -fv $I.html
-done
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if [ ! -f words.txt ]; then
+	for I in {1..500}
+	do
+		URL='https://scholar.google.com/citations?user=qQLlBH4AAAAJ&hl=en&oi=ao&cstart='
+		wget -nc --output-document "$I.html" $URL$I
+		cat $I.html | tr ">" "\n" >> temp.html
+		rm -fv $I.html
+	done
+fi
 
 grep 'class="gsc_a_at"' -A1 temp.html | cut -d'"' -f1 | cut -d'=' -f2 | cut -d"<" -f1 | sort  | uniq | xargs echo | tr '[:upper:]' '[:lower:]' > words.txt
 rm -fv temp.html
 
 python generate_word_cloud_for_bob_murphy.py
-rm -fv words.txt
+wc -w words.txt
 
 DIRECTORY='images'
 if [ ! -d $DIRECTORY ]; then
