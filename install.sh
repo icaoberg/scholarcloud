ea198bae67f5900e6be1b32f384f062ca2101a56
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2015-11-11 16:58:29 -0500

    Updated installation script

--- install.sh
+++ install.sh
@@ -3,8 +3,12 @@
 virtualenv --system-site-packages .
 source ./bin/activate
 pip install cython
+pip install -U ipython
+pip install tabulate
+pip install textract
 cd word_cloud
 python setup.py install
 cd ..
+
 
 deactivate
