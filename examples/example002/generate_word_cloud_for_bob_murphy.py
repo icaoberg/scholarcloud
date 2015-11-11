b621294898294175b6fb600d44d3783a3d902e7e
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2015-11-11 12:10:39 -0500

    Updated example002

---
+++ examples/example002/generate_word_cloud_for_bob_murphy.py
@@ -0,0 +1,50 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from os import path
+import os
+from scipy.misc import imread
+import matplotlib.pyplot as plt
+import random
+
+import urllib
+
+from wordcloud import WordCloud, STOPWORDS
+
+#change this to your taste
+dpi = 3000
+
+#i used wc to count the number of words in little_women.txt
+list_of_number_of_words = [25,50,100,250,500,1000]
+
+def grey_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
+    return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
+
+d = os.getcwd()
+
+#these are the extracted words from bob murphy's article titles
+filename = 'words.txt'
+text = open(filename).read()
+
+stopwords = STOPWORDS.copy()
+
+width=2400
+height=2200
+
+for number_of_words in list_of_number_of_words:
+	wordcloud = WordCloud(max_words=number_of_words, stopwords=stopwords, margin=0,
+	      	        random_state=1, width=width, height=height).generate(text)
+
+	filename='words' + str(number_of_words).zfill(6) + '.png'
+	image = wordcloud.to_image()
+	image.save( filename, "PNG" )
