ef24f1f9b190df1029203abbfd7ae565193ba603
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2015-11-11 23:09:04 -0500

    Updated script for example002

--- examples/example002/generate_word_cloud_for_bob_murphy.sh
+++ examples/example002/generate_word_cloud_for_bob_murphy.sh
@@ -13,6 +13,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+ if [ -z `which pdftotext` ]; then
+   echo "pdftotext is not installed. Install first and then try again."
+   exit
+ fi
+
+WORDS='words_'`date +"%m-%d-%Y"`'.txt'
+
 echo "Downloading articles"
 for FILE in $(cat links.out)
 do
@@ -35,12 +42,37 @@ do
         fi
 done
 
-python generate_word_cloud_for_bob_murphy.py
-wc -w words.txt
+if [ -f temp.html ]; then
+	rm -fv temp.html
+fi
 
+python generate_word_cloud_for_bob_murphy.py
+
+if [ -f words.txt ]; then
+	wc -w words.txt
+fi
+mv words.txt $WORDS
+
+#images
 DIRECTORY='images'
 if [ ! -d $DIRECTORY ]; then
 	mkdir $DIRECTORY
 fi
 
-mv -v *.png images
+COUNT=`ls -1 *.png 2>/dev/null | wc -l`
+if [ $COUNT != 0 ]
+then
+	mv -v *.png $DIRECTORY
+fi
+
+#words
+DIRECTORY='words'
+if [ ! -d $DIRECTORY ]; then
+	mkdir $DIRECTORY
+fi
+
+COUNT=`ls -1 *.txt 2>/dev/null | wc -l`
+if [ $COUNT != 0 ]
+then
+	mv *.txt $DIRECTORY
+fi
