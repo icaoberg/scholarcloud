2a77f957e0a952f6d70babf8f538cb1a0de85e3d
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2015-11-14 23:24:29 -0500

    Updated installation script to include Pillow

--- install_virtualenv.sh
+++ install_virtualenv.sh
@@ -4,6 +4,7 @@ virtualenv --system-site-packages .
 source ./bin/activate
 pip install cython
 pip install -U ipython
+pip install pillow==2.8.2
 pip install tabulate
 pip install textract
 pip install wordcloud
